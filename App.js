e17d0f9d3380c1b0274a685e40ddef4ca41a8a38
Valente90045 <logictrue@hotmail.com>
2021-01-19 13:07:48 -0300

    Alteraçoes feitas até 18/01

--- App.js
+++ App.js
@@ -1,26 +1,27 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, Button} from 'react-native';
-import styled from 'styled-components';
+import {SafeAreaView, View, Text, Button} from 'react-native';
+import styled from 'styled-components/native';
 
 const Page = styled.SafeAreaView`
-    flex=1;
-    align-items:center;    
+  flex: 1;
+  align-items: center;
 `;
 const HeaderText = styled.Text`
   font-size: 25px;
 `;
 const Input = styled.TextInput`
-    width:90%;
-    height:50px;
-    font-size: 18px;
-    background-color:#EEE
-    margin-top:20px;
-    border-radius:10px;
-    padding:10px;
+  width: 90%;
+  height: 50px;
+  font-size: 18px;
+  background-color: #eee;
+  margin-top: 20px;
+  border-radius: 10px;
+  padding: 10px;
 `;
 const CalcButton = styled.Button`
   margin-top: 10px;
   font-size: 15px;
+  padding-top: 10px;
 `;
 const PctArea = styled.View`
   flex-direction: row;
@@ -33,7 +34,7 @@ const ResultArea = styled.View`
   background-color: #eee;
   padding: 20px;
   justify-content: center;
-  align-center: center;
+  align-items: center;
 `;
 const ResultItemTitle = styled.Text`
   font-size: 18px;
@@ -42,6 +43,7 @@ const ResultItemTitle = styled.Text`
 const ResultItem = styled.Text`
   font-size: 15px;
 `;
+
 export default () => {
   const [bill, setBill] = useState('');
   const [tip, setTip] = useState(0);
@@ -56,20 +58,18 @@ export default () => {
 
   useEffect(() => {
     calc();
-    [pct];
-  });
+  }, [ pct ]);
 
   return (
     <Page>
-      <HeaderText>CALCULADORA DE GORGETA</HeaderText>
+      <HeaderText>Calculadora de Gorjeta</HeaderText>
       <Input
         PlaceHolder="Quanto deu a conta?"
         PlaceHolderTextcolor="#000"
         KeyboardType="numeric"
         Value={bill}
-        OnChangeText={(n) => setBill(n)}
+        OnChangeText={n => setBill(n) }
       />
-      <CalcButton title="Calcular" onPress={calc} />
 
       <PctArea>
         <PctItem title="5%" onPress={() => setPct(5)} />
@@ -78,7 +78,9 @@ export default () => {
         <PctItem title="20%" onPress={() => setPct(20)} />
       </PctArea>
 
-      {tip > 0 && (
+      <CalcButton title="Calcular" onPress={calc} />
+
+      {tip > 0 &&
         <ResultArea>
           <ResultItemTitle> Valor da Conta: </ResultItemTitle>
           <ResultItem>R$ {parseFloat(bill).toFixed(2)} </ResultItem>
@@ -87,7 +89,7 @@ export default () => {
           <ResultItemTitle> Valor Total: </ResultItemTitle>
           <ResultItem>R$ {(parseFloat(bill) + tip).toFixed(2)} </ResultItem>
         </ResultArea>
-      )}
+      }
     </Page>
   );
-};
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Este aplicativo faz parte do Curso de REact-Native da B7Web
+
